149f281e0432a27a69634955e7880260111ce485
mlss1 <167926203+mlss1@users.noreply.github.com>
2024-04-30 17:01:46 +0000

    modificacion single, main, agregar carpetas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
+import { useNavigate } from "react-router-dom";
+import Proyectos from './data/proyectos.json'
 
 const feather = require('feather-icons');
 
@@ -6,37 +8,33 @@ setTimeout(() => {
     feather.replace();
 }, 1000);
 
-const gatos = [
-  {
-      name: "Diseño 1",
-      description: "Descripcion Diseño 1"
-  },
-  {
-      name: "Diseño 2",
-      description: "Descripcion Diseño 2"
-  },
-  {
-      name: "Diseño 3",
-      description: "Descripcion Diseño 3"
-  },
-];
+
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (slug) => {
+      navigate("/detalle/" + slug);
+  }
+
   return <main>
       <Carousel>
           {
-              gatos.map(it => {
-                  return (<Carousel.Item>
-                      <img className='w-100 dark-image' src='https://mir-s3-cdn-cf.behance.net/project_modules/1400/6fba5967789515.5b462a43c6a69.jpg'></img>
+              Proyectos.map(it => {
+                  return (<Carousel.Item onClick={(e) => {
+                      e.preventDefault(); handleClick(it.slug)
+                  }}>
+                      <img className='w-100 dark-image' src={it.picture}></img>
                       <Carousel.Caption>
                           <h3>{it.name}</h3>
                           <p>{it.description}</p>
+                          <button className="btn btn-primary" >Ver detalle</button>
                       </Carousel.Caption>
                   </Carousel.Item>);
               })
           }
       </Carousel>
-  </main>;
+  </main >;
 }
 
 export default Main;
--- src/single.js
+++ src/single.js
@@ -1,6 +1,15 @@
+import Proyectos from './data/proyectos.json'
+
 const Single = () => {
+
+    const currentRoute = window.location.pathname;
+    const proyecto = Proyectos.find(it => currentRoute.includes(it.slug))
+
     return (<>
-        <h1>Pagina simple</h1>
+        <h1>{proyecto.name}</h1>
+        <hr></hr>
+        <img src={proyecto.picture}></img>
+        <p>{proyecto.description}</p>
     </>);
 }
 
